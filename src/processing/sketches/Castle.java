95bd83a9aa15a2c38c5722837151eca1a049c211
stevemurimi <stevemurimi44@gmail.com>
2025-08-22 15:25:34 +0300

    done with the project

---
+++ src/processing/sketches/Castle.java
@@ -0,0 +1,88 @@
+package processing.sketches;
+
+public class Castle {
+    private String name;
+    private int population;
+    private String zipcode;
+    private float area;
+    private String landmark;
+    private boolean isCapital;
+    private float latitude;
+    private float longitude;
+
+    public Castle(String name, int population, String zipcode, float area,
+                  String landmark, boolean isCapital, float latitude, float longitude) {
+        this.name = name;
+        this.population = population;
+        this.zipcode = zipcode;
+        this.area = area;
+        this.landmark = landmark;
+        this.isCapital = isCapital;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public String getLandmark() {
+        return landmark;
+    }
+
+    public void setLandmark(String landmark) {
+        this.landmark = landmark;
+    }
+
+    public boolean isCapital() {
+        return isCapital;
+    }
+
+    public void setCapital(boolean capital) {
+        isCapital = capital;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+}
--- src/processing/sketches/Main.java
+++ src/processing/sketches/Main.java
@@ -9,6 +9,9 @@ public class Main extends PApplet {
     public PImage starImage;
     public PImage mapImage;
 
+    private Castle castle1;
+    private Castle castle2;
+
     public void settings() {
         castleImage = loadImage("castle.png");
         starImage = loadImage("CapitolStar.png");
@@ -21,18 +24,46 @@ public class Main extends PApplet {
         background(0);
         imageMode(CENTER);
         textAlign(CENTER);
+
+        castle1 = new Castle("Castle A", 1000, "12345",
+                2500.5f, "Great Hall", true,
+                40.7128f, 174.0060f);
+
+        castle2 = new Castle("Castle B", 500, "67890",
+                5500.0f, "Royal Gardens", false,
+                234.0522f, 318.2437f);
     }
 
     public void draw() {
-        image(mapImage, 0,0, width, height);
+        image(mapImage, width / 2,height / 2, width, height);
 
-        //call drawCastle for each castle here
+        drawCastle(castle1);
+        drawCastle(castle2);
     }
 
-//  Uncomment when ready to draw castles
-//    public void drawCastle(Castle castle) {
-//
-//    }
+    public void drawCastle(Castle castle) {
+        float sideLength = (float) Math.sqrt(castle.getArea());
+        image(castleImage, castle.getLatitude(), castle.getLongitude(), sideLength, sideLength);
+
+        if (castle.isCapital()) {
+            image(starImage, castle.getLatitude(), castle.getLongitude() - sideLength / 2,
+                     sideLength / 4, sideLength / 4);
+        }
+
+        if (dist(castle.getLatitude(), castle.getLongitude(), mouseX, mouseY) < sideLength / 2) {
+            textSize(14);
+            fill(0);
+            text(castle.getName(), castle.getLatitude(), castle.getLongitude() - sideLength / 2 - 10);
+            text("Population: " + castle.getPopulation(), castle.getLatitude(), castle.getLongitude() + 10);
+            text("Zipcode: " + castle.getZipcode(), castle.getLatitude(), castle.getLongitude() + 30);
+            text("Area: " + castle.getArea() + " sq. units", castle.getLatitude(), castle.getLongitude() + 50);
+            text("Landmark: " + castle.getLandmark(), castle.getLatitude(), castle.getLongitude() + 70);
+            text("Coordinates: (" + castle.getLatitude() + ", " + castle.getLongitude() + ")",
+                 castle.getLatitude(), castle.getLongitude() + 90);
+            text("Capital: " + (castle.isCapital() ? "Yes" : "No"),
+                 castle.getLatitude(), castle.getLongitude() + 110);
+        }
+    }
 
     public static void main(String... args) {
         PApplet.main("processing.sketches.Main");
